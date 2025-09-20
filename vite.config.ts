3621e79c7581933b8cdd9c96cb5fc25cd092a313
Mario Hernández Sánchez <mhersan@gmv.com>
2025-09-20 05:00:32 +0200

    Updated config for custom site.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mystic-curly/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
